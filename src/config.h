c4693db9193bf3a05cdb6f5e17b2c5fae8e3ac1d
Queen Vinyl Da.i'gyu-Kazotetsu <queengooborg@gmail.com>
2026-09-22 23:47:28 -0700

    Release forked v1.3.0

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 #include <Preferences.h>
 
-#define FIRMWARE_VERSION "1.2.0"
+#define FIRMWARE_VERSION "1.3.0-queengooborg"
 #define OTA_REPO \
   "queengooborg/BambuTagger-AMS-C"  // "VID-PRO/BambuTagger-AMS-C" -- XXX restore once forked
                                     // changes are merged upstream
